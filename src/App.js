0a946bd7dc6cb2e083892d627c6801c8e7ffd5ae
Mariano Guerra <devmarianoguerra@gmail.com>
2020-06-08 18:20:30 -0500

    add react-bootstrap package and imported in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
-import ToDos from './components/toDos'
+import React from "react";
+import "./App.css";
+import ToDos from "./components/toDos";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-      <ToDos/>
+      <ToDos />
     </>
   );
 }
